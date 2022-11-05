d47b00da91f01fc8caf8b71016721a0d42cead40
a100554 <a100554@alunos.uminho.pt>
2022-11-05 16:22:59 +0000

    ReadFile Complete

--- src/main.c
+++ src/main.c
@@ -6,8 +6,164 @@
 #include "parser.h"
 #include "queries.h"
 
+#define MAX_FILE_NAME_SZ    1024
+#define MAX_LINE_SZ         1024
+#define MAX_ARGS            10
+
+#define ERR_OK 0
+#define ERR_NOT_ENOUGH_ARGS 1
+#define ERR_CANT_OPEN_RESULTS_FILE 2
+#define ERR_QUERY_NOT_IMPLEMENTED 3
+
+char *errors[] = {
+    "OK",
+    "No arguments specified.\nUSAGE: <Query ID> [Args]",
+    "Cannot open results file",
+    "Query not implemented"
+ };
+
+int saveResult(char *resultFileName, char *resultLine){
+    int resultCode = ERR_OK;
+
+    FILE *fresult = fopen( resultFileName, "w+" );
+    if(fresult){
+        if( resultLine != NULL ){
+            fprintf(fresult, "%s\n", resultLine );
+        }
+        fclose(fresult);
+    }
+    else{
+        resultCode = ERR_CANT_OPEN_RESULTS_FILE;
+    }    
+}
+
+int parseCommand( int argc, char *argv[], User *u, Ride *r, Driver *d, char *resultFileName ){
+    int resultCode = ERR_OK;
+    struct userQ1 *resultQ1 = NULL;
+
+    if( argc < 3 ){
+        return ERR_NOT_ENOUGH_ARGS;
+        //printf("No arguments specified.\nUSAGE: <Query ID> [Args]");
+    }
+    else{
+        int queryId = atoi(argv[1]);
+        switch ( queryId ) {
+            case 1:
+                resultQ1 = q1(argv[2], u, r, d);
+                if( resultQ1 != NULL ){
+                    char resultLine[ MAX_LINE_SZ ];
+                    sprintf( resultLine, "%s;%c;%d;%0.3f;%d;%0.2f\n", 
+                            resultQ1->name,
+                            resultQ1->gender,
+                            resultQ1->age,
+                            resultQ1->avg_score,
+                            resultQ1->travel_count,
+                            resultQ1->total_cost );
+                    free(resultQ1);
+                    //
+                    if( resultFileName == NULL ){
+                        printf("%s\n", resultLine);
+                    }
+                    else{
+                        saveResult(resultFileName, resultLine);
+                    }
+                }
+                else{
+                    saveResult(resultFileName, NULL );
+                }
+                break;
+            
+            default:
+                resultCode = ERR_QUERY_NOT_IMPLEMENTED;
+                //printf("%s: Not implemented\n", argv[1]);
+                break;
+        }
+        return resultCode;
+    }
+}
+
+int processCommandFile (FILE *file, User* u, Ride *r, Driver *d ){
+    char *args[MAX_ARGS];
+    char line[MAX_LINE_SZ];
+    int lineNumber = 0;
+    
+    if( file ){
+        while (fgets(line, sizeof(line), file)) {
+            lineNumber++;
+            char *tok = strtok( line, " " );
+            int i = 0;
+            args[i++] = NULL; // just to keep compatiblity with command line
+            while (tok != NULL ){
+                int strsz=strlen(tok);
+                args[i] = malloc(sizeof(char)*strsz + 1);
+                int ii=0;
+                char c = tok[ii];
+                while( c !='\0' && c != '\n' ){
+                    args[i][ii] = c;
+                    c = tok[++ii];
+                }
+                args[i++][ii] = '\0';
+                tok = strtok( NULL, " " );
+            }
+            char resultFileName[ MAX_FILE_NAME_SZ ];
+            sprintf(resultFileName,"results/command%d_output.txt", lineNumber);
+            // i=number os elements read
+            int parseResult = parseCommand(i, args, u, r, d, resultFileName);
+            // Frees args
+            for( int ii=0; ii<i; ii++ ){
+                if( args[ii]){
+                    free(args[ii]);
+                    args[ii]=NULL;
+                }
+            };
+            if( parseResult ){
+                printf("Line %d: %s\n", lineNumber, errors[parseResult]);
+            }
+        }
+    }
+    return lineNumber;
+}
+
 
 int main( int argc, char *argv[] )  {
+    // Batch mode
+    // argv[1] --> Input file folder
+    // argv[2] --> command file
+    //
+    // Single mode --> Menu
+
+    if( argc < 3 ){
+        printf("No arguments specified.\nUSAGE: <Input files folder> <Command file>");
+    }
+    else{
+        char *inputFolder = argv[1];
+        char *commandFile = argv[2];
+        
+        char filename[MAX_FILE_NAME_SZ];
+        sprintf( filename, "%s/users.csv", inputFolder );
+        User *u = read_user(filename);
+        sprintf( filename, "%s/drivers.csv", inputFolder );
+        Driver *d = read_driver(filename);
+        sprintf( filename, "%s/rides.csv", inputFolder );
+        Ride *r = read_ride( filename );
+
+        //User *u = read_user("Dataset_Fase1/users.csv");
+        //Driver *d = read_driver("Dataset_Fase1/drivers.csv");
+        //Ride *r = read_ride( "Dataset_Fase1/rides.csv" );
+
+        // Read command file and parse + execute
+        FILE *fp = fopen(commandFile, "r");
+        if( fp != NULL ){
+            processCommandFile( fp, u, r, d );
+            fclose(fp);
+        }
+        else{
+            printf("Cannot open command file: %s\n", commandFile);
+        }
+    }
+
+
+    /*
     if( argc <=1 ){
         printf("No arguments specified.\nUSAGE: <Query ID> [Args]");
     }
@@ -38,46 +194,6 @@ int main( int argc, char *argv[] )  {
                 break;
         }
     } 
-
-//int main(){
-
-    /*
-    while (u != NULL){
-        printf("%s\t", u->username);
-        printf("%s\t", u->name);
-        printf("%c\t", u->gender);
-        printf("%02d/%02d/%04d\t", u->birth.tm_mday, u->birth.tm_mon, u->birth.tm_year);
-        printf("%02d/%02d/%04d\t", u->acount.tm_mday, u->acount.tm_mon, u->acount.tm_year);
-        printf("%s", u->status);
-        u = u->next;
-    }
-    
-    while (d != NULL){
-        printf("%s\t", d->id);
-        printf("%s\t", d->name);
-        printf("%02d/%02d/%04d\t", d->birth.tm_mday, d->birth.tm_mon, d->birth.tm_year);
-        printf("%c\t", d->gender);
-        printf("%s\t", d->classe);
-        printf("%s\t", d->plate);
-        printf("%s\t", d->city);
-        printf("%02d/%02d/%04d\t", d->acount.tm_mday, d->acount.tm_mon, d->acount.tm_year);
-        printf("%s\n", d->status);
-        d = d->next;
-    }
-    
-    while (r != NULL){
-        printf("%s\t", r->id);
-        printf("%02d/%02d/%04d\t", r->date.tm_mday, r->date.tm_mon, r->date.tm_year);
-        printf("%s\t", r->driver);
-        printf("%s\t", r->user); 
-        printf("%s\t", r->city);
-        printf("%d\t", r->distance);
-        printf("%d\t", r->sUser);
-        printf("%d\t", r->sDriver);
-        printf("%03f\t", r->tip);
-        printf("%s\n", r->comment);
-        r = r->next;
-    }
     */
     return 0;
 }
--- src/queries.c
+++ src/queries.c
@@ -83,7 +83,7 @@ struct userQ1 *q1(char *username, User *users, Ride *rides, Driver *drivers){
 
     if (atoi(username) == 0){
         User *u = getUser( users, username);
-        if( u != NULL){
+        if( u != NULL && strcasecmp(u->status,"active\n") == 0){
             result = malloc(sizeof(struct userQ1));
             
             strncpy( result->username, u->username, MAX_SZ_NAME);
@@ -113,7 +113,7 @@ struct userQ1 *q1(char *username, User *users, Ride *rides, Driver *drivers){
     }
     else{
         Driver *d = getDriver( drivers, username);
-        if( d != NULL){
+        if( d != NULL && strcasecmp(d->status,"active\n") == 0){
             result = malloc(sizeof(struct userQ1));
             
             strncpy( result->username, d->id, MAX_SZ_NAME);
